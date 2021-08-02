dfe49750941c8cc1a643af171212fc3811fe4d5d
crusat <crusat@yandex.ru>
2021-08-02 16:10:09 +0300

    init

---
+++ backend/garpix_course/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'garpix_course.apps.GarpixCourseConfig'
---
+++ backend/garpix_course/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GarpixCourseConfig(AppConfig):
+    name = 'garpix_course'
+    verbose_name = 'GarpixCourse'
